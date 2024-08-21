1a63e06a870c4219428fb359c6351d7c1ebc2f01
Deekshith <deekshith3538@gmail.com>
2024-08-21 13:38:41 +0530

    Placeholder Added

--- xdict/src/Components/Home.jsx
+++ xdict/src/Components/Home.jsx
@@ -46,7 +46,7 @@ export default function Dictionary() {
 
         <button type="submit">Search</button>
       </form>
-      {reply && <>Definition:<p>{reply}</p></>}
+      <>Definition:<p>{reply}</p></>
     </div>
   );
 }
