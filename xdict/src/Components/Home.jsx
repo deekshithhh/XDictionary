7520fdc035ad907fffc9b5fb3b448398ea440e26
Deekshith <deekshith3538@gmail.com>
2024-08-21 12:44:05 +0530

    Test Case handling

--- xdict/src/Components/Home.jsx
+++ xdict/src/Components/Home.jsx
@@ -42,7 +42,7 @@ export default function Dictionary() {
 
         <button type="submit">Search</button>
       </form>
-      {reply && <div>Definition:<p>{reply}</p> </div>}
+      {reply && <>Definition:<p>{reply}</p> </>}
     </div>
   );
 }
