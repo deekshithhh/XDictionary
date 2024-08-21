8e0a585ebb20404de70c4004459a6ac66ba9a336
Deekshith <deekshith3538@gmail.com>
2024-08-21 12:24:08 +0530

    Initial Commit with Task

---
+++ xdict/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Dictionary from './Components/Home';
+function App() {
+  return (
+    <div className="App">
+     <Dictionary/>
+    </div>
+  );
+}
+
+export default App;
---
+++ xdict/src/Components/Home.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Dictionary() {
+  const [text, setText] = useState("");
+  const [reply, setReply] = useState("");
+
+  const DictData = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+
+  const Searchdict = (SearchData) => {
+    const response = DictData.find(
+      (item) => SearchData.toLowerCase() == item.word.toLocaleLowerCase()
+    );
+
+    if (response != undefined) {
+      setReply(response.meaning);
+    } else {
+      setReply("Word not found in the dictionary.");
+    }
+  };
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+    Searchdict(text);
+  };
+
+  return (
+    <div>
+      <h1>Dictionary App</h1>
+      <form onSubmit={handlesubmit}>
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        {/* <input type='text' onChange={(e) => setText(e.target.value)} /> */}
+
+        <button type="submit">Search</button>
+      </form>
+      {reply && <div>Definition: {reply}</div>}
+    </div>
+  );
+}
